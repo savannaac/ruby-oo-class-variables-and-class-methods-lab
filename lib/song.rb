88e2047c9d561f55fd1f3915381c638df74a1f91
savanna chiang <savannaac@gmail.com>
2021-02-12 22:23:34 -0600

    Class Variables/Methods song lab-complete

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+
+        @@count += 1
+        @@artists << artist 
+        @@genres << genre
+    end
+
+    def self.count 
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.each.with_object(Hash.new(0)) { |w, c| c[w] += 1 }
+    end
+
+    def self.artist_count
+        @@artists.each.with_object(Hash.new(0)) { |w, c| c[w] += 1 }
+    end
+
+end 
